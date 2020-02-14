37d4f8e2a0f883dffe71624fc0cb9535f2e7fb94
Student <example@example.com>
2020-02-14 14:24:14 -0800

    accesses API and takes the name of a doctor

--- src/backend.js
+++ src/backend.js
@@ -1,10 +1,13 @@
+import {specificDoctor, condition} from './main.js';
+
 export class Doctor {
-  
+
   async getDoctorName() {
     try {
-      const response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json&words=1&paragraphs=1`); //sample API Will Change to Doctors
+      const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=WA&skip=0&limit=10&user_key=9c2ef529d0519e124391cca9ce52f975`); //sample API Will Change to Doctors
       let jsonifiedResponse = await response.json();
-      return jsonifiedResponse;
+      console.log(jsonifiedResponse.data[0].profile.first_name);
+      return jsonifiedResponse.data[0].profile.first_name;
     } catch(error) {
       console.error("There was an error handling your request: " + error.message);
     }
--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import 'bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
 import { Doctor } from './backend.js';
-
+// export let condition = ($("#condition").val());
+// export let specificDoctor= ($("#specificDoctor").val());
 $(document).ready(function() {
   let doc = new Doctor();
   $('#getDinoText').click(function() {
   let condition = ($("#condition").val());
-  let inputstr = ($("#specificDoctor").val());
+  let specificDoctor= ($("#specificDoctor").val());
   console.log(condition);
-  console.log(inputstr);
+  console.log(specificDoctor);
 
     (async () => {
       const response = await doc.getDoctorName();
