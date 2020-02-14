42c44bc085c6d52cb030cb06b22392854763ec51
Student <example@example.com>
2020-02-14 15:56:54 -0800

    adds security layer to call funcitons

--- src/backend.js
+++ src/backend.js
@@ -4,7 +4,7 @@ export class Doctor {
 
   async getDoctorName(condition, location) {
     try {
-      const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${location}&skip=0&limit=10&user_key=9c2ef529d0519e124391cca9ce52f975`); //sample API Will Change to Doctors
+      const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${location}&skip=0&limit=10&user_key=${process.env.API_KEY}``); //sample API Will Change to Doctors
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse.data[0].profile.first_name
     } catch(error) {
