6489f9f78b79ca886ab764743a31fcd1de8d9c88
Student <example@example.com>
2020-02-15 16:04:00 -0800

    properly targets needed variables

--- src/backend.js
+++ src/backend.js
@@ -17,7 +17,12 @@ export class Doctor {
       const responseList = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${location}&skip=0&limit=100&user_key=9c2ef529d0519e124391cca9ce52f975`); //sample API Will Change to Doctors
       let jsonifiedResponseList = await responseList.json();
       let listamount = jsonifiedResponseList.data.length;
-      console.log(jsonifiedResponseList.data.length);
+      let lastName = "Last Name " + jsonifiedResponseList.data[0].profile.last_name;
+      console.log(lastName);
+      console.log("Website" + jsonifiedResponseList.data[0].practices[0].website);
+      console.log( "Accepting New Patients " + jsonifiedResponseList.data[0].practices[0].accepts_new_patients);
+      console.log("Phone Number " + jsonifiedResponseList.data[0].practices[0].phones[0].number);
+      console.log("Address " + jsonifiedResponseList.data[0].practices[0].visit_address.street + " " + jsonifiedResponseList.data[0].practices[0].visit_address.city + " " +  jsonifiedResponseList.data[0].practices[0].visit_address.state + " " + jsonifiedResponseList.data[0].practices[0].visit_address.zip);
       return listamount;
     } catch(error) {
       console.error("There was an error handling your request: " + error.message);
